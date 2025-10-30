3fa3ac85c655aedf5cc45b18ccb9b109934d2dc8
DevP4osckbed <devbananastudios@gmail.com>
2025-10-30 17:55:17 +0000

    USAGE

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+    // Open in default browser
+    require('child_process').exec(`"$BROWSER" http://localhost:${port}`);
+});
